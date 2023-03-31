1d6d8e5cdbea04d23261fc095afb77685891f7ca
KIBET KORIR <93614316+Kibet-g@users.noreply.github.com>
2023-03-31 11:21:51 +0300

    This is an update for my web application Boom AI
    Boom AI

--- client/script.js
+++ client/script.js
@@ -1,63 +1,120 @@
-import bot from './assets/bot.svg, user.svg';
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
- const form =document.querySelector('form');
- const chatContainer =document.querySelector(chatContainer);
-let loadInterval;
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
+let loadInterval
 
+function loader(element) {
+    element.textContent = ''
 
-//OUR JAVASCRIPT FUNCTIONALITY TO LOAD THE DOTS AFTER EVERY 300 SECONDS
-function loader(element)
-{
-  element.textContent='';
-  loadInterval=setInterval(()=>{
-  element.textContent +='.';
-  
-  if(element.textContent==='...'){
-    element.textContent='';
-  }
-},300)
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
 
-//FUNCTION TO TYPE TEXT BY TEXT
-function typetext(element, text){
+function typeText(element, text) {
+    let index = 0
 
-  let index=0;
-  let interval=setInterval(()=>{
-    if (index<text.length)
-    {
-      element.innerHTML += text.charAt(index);
-      index++
-    } else{
-      clearInterval(interval);
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++;
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
     }
-  },20)
-  
 }
-//FUNCTION TO CREATE A UNIQUE ID FOR EVERY SINGLE MESSAGE
-function generateUniqueId()
-{
-  const timestamp= Date.now();
-  const randomNumber=Math.random();
-  const hexadecimalString= randomNumber.toString;
-  return 'id-${timestamp}-${hexadecimalString}'
-}  
- //A FUNCTION TO IMPLEMENT A  FROM B;ACK TO GREYMESSAGING
- function chatStripe (isAi, value, uniqueId){
-  return (
-    
-    <div class="wrapper ${isAi && 'ai'}">
-    <div class="chat">
-    <div className="profile">
-    <img
-    src="${isAi ? bot : user}"
-    alt="${isAi ? 'bot' : 'user'}"
-    />
-    </div>
-    <div class="message" id={uniqueId}>${value}</div>
-    </div>
-    </div>
-  )
- }
 
- 
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
